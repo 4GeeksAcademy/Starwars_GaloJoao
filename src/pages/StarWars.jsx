47234c76d32a1ff15bd85d079913392abae41aba
GJoao24 <122391147+GJoao24@users.noreply.github.com>
2025-09-23 18:23:53 +0000

    Modificacion de start wars

--- src/pages/StarWars.jsx
+++ src/pages/StarWars.jsx
@@ -1,36 +1,64 @@
 import { use, useEffect, useState } from "react"
+import useGlobalReducer from "../hooks/useGlobalReducer"
 
 export const StarWars = () => {
-    const [personajes, setNombresPersonaje] = useState([])
-    const [vehiculov] = useState([])
-    const [planetas, setPlanetas] = useState([])
+
+    const {store, dispatch} = useGlobalReducer()
+
+
+
     useEffect(() => {
-        getpersonajes()
+        getPersonajes(),getVehiculos(),
+        getPlanetas()
     },)
     const getPersonajes = async () => {
         const result = await fetch("https://swapi.tech/api/people")
         const data = await result.json()
-        setNombresPersonaje(data.results)
+        dispatch({"type":'set_peoples',
+            "payload": data.results
+        })
 
     }
     const getVehiculos = async () => {
-        const result = await fetch("https://swapi.tech/api/vehiculos")
+        const result = await fetch("https://swapi.tech/api/vehicle")
         const data = await result.json()
-        setNombresPersonaje(data.results)
-
+        dispatch({"type":'set_vehicle',
+            "payload": data.results
+        })
     }
 
         const getPlanetas = async () => {
-        const result = await fetch("https://swapi.tech/api/planetas")
+        const result = await fetch("https://swapi.tech/api/planets")
         const data = await result.json()
-        setNombresPersonaje(data.results)
+        dispatch({"type":'set_planets',
+            "payload": data.results
+        })
 
     }
 
 
     return (
         <div>
+            <h3>Personajes</h3>
+            {store.people.map((p)=> {
+                return (
+                    <p>{p.name}</p>
+                )
+            })}
 
+            <h3>Planetas</h3>
+            {store.planets.map((p)=> {
+                return (
+                    <p>{p.name}</p>
+                )
+            })}
+
+            <h3>Vehiculos</h3>
+            {store.vehicles.map((p)=> {
+                return (
+                    <p>{p.name}</p>
+                )
+            })}
         </div>
     )
 }
--- src/store.js
+++ src/store.js
@@ -1,31 +1,30 @@
 export const initialStore=()=>{
   return{
-    message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
+   planets: [],
+   people: [],
+   vehicles: []
   }
 }
 
 export default function storeReducer(store, action = {}) {
   switch(action.type){
-    case 'add_task':
 
-      const { id,  color } = action.payload
+      case 'set_planets':
+        return {
+          ...store,
+          planets: action.payload
+        }
+      case 'set_vehicles':
+        return {
+          ...store,
+          vehicles: action.payload
+        }
+      case 'set_peoples':
+        return {
+          ...store,
+          people: action.payload
+        }
 
-      return {
-        ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
-      };
     default:
       throw Error('Unknown action.');
   }    
